5e95b4ba98ed44c478ecf5ecbfb09c0089586459
root <root@box.ericdiaz.dev>
2019-06-18 15:34:29 -0700

    update

--- static/build/precache-manifest.4a306b68a392de401dd52f01a0ca8648.js
+++ static/build/precache-manifest.4a306b68a392de401dd52f01a0ca8648.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c5fb5e0b5e4478efb4c813f2749dd9d6",
+    "revision": "8a367a3a9358224f882bbeaaf38bcb9a",
     "url": "/index.html"
   },
   {
-    "revision": "5762ee4d7c36465ee1e5",
-    "url": "/static/css/main.7117f1a6.chunk.css"
+    "revision": "4f378519fa87030137db",
+    "url": "/static/css/main.49af797b.chunk.css"
   },
   {
     "revision": "2384846391663d638e5c",
     "url": "/static/js/2.656f2b49.chunk.js"
   },
   {
-    "revision": "5762ee4d7c36465ee1e5",
-    "url": "/static/js/main.8e382468.chunk.js"
+    "revision": "4f378519fa87030137db",
+    "url": "/static/js/main.65a2810d.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
