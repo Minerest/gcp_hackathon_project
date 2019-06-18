2e06737eced7600c745c9c0dabe6b2ffe06e5fb9
Eric Diaz <eric.diaz13@yahoo.com>
2019-06-18 15:33:03 -0700

    Merge branch 'react' of /home/ericd/opt/gcp_hackathon_project with conflicts.

--- baltimorecity/add_baltimore_city.py
+++ baltimorecity/add_baltimore_city.py
@@ -12,99 +12,6 @@ import csv
 import datetime
 
 
-
-#
-# def main():
-#
-#     db = modals.CloudDB()
-#
-#     modals.create_table(db.engine)
-#
-#     list_of_data = []
-#     first_pass = True
-#     rows = 0
-#     junk_in_csv = "()"
-#     date_bound_lower = datetime.datetime(2014, 1, 1)
-#     date_bound_upper = datetime.datetime(2016, 1,1)
-#     cnt = 0
-#     with open("cityoftacoma5960") as f:
-#         baltimore_crime_data = csv.reader(f)
-#         for row in baltimore_crime_data:
-#             rows += 1
-#             if first_pass:
-#                 first_pass = False
-#                 continue
-#
-#             dt = row[2]
-#             loc_data = row[4]
-#             crime_type = row[1]
-#             try:
-#                 _, lat_lon_pair = loc_data.split("(")
-#                 lat, lon = lat_lon_pair.split(",")
-#
-#             except:
-#                 cnt += 1
-#                 continue
-#
-#             for junk in junk_in_csv:
-#                 lat = lat.replace(junk, "")
-#                 lon = lon.replace(junk, "")
-#             try:
-#                 lat = float(lat)
-#                 lon = float(lon)
-#             except:
-#                 cnt += 1
-#                 continue
-#             data_to_enter = modals.get_location_schematic()
-#
-#             dt = datetime.datetime.strptime(dt, "%m/%d/%Y")
-#
-#             data_to_enter["date"] = dt
-#             try:
-#                 data_to_enter["longitude"] = float(lon)
-#                 data_to_enter["latitude"] = float(lat)
-#
-#             except Exception as e:
-#                 print(e, row)
-#                 continue
-#
-#             if "assault" in crime_type.lower():
-#                 data_to_enter["assault"] += 1
-#
-#             elif "murder" in crime_type.lower():
-#                 data_to_enter["murder"] += 1
-#
-#             elif "theft" in crime_type.lower() or "larceny" in crime_type.lower()\
-#                          or "burglary" in crime_type.lower():
-#                 data_to_enter["theft"] += 1
-#
-#             elif "rape" in crime_type.lower():
-#                 data_to_enter["rape"] += 1
-#
-#             elif "grand" in crime_type.lower() and \
-#                  "theft" in crime_type.lower() and \
-#                  "auto"  in crime_type.lower():
-#                 data_to_enter["gta"] += 1
-#
-#             elif "robbery" in crime_type.lower():
-#                 data_to_enter["robbery"] += 1
-#
-#             else:
-#                 data_to_enter["other"] += 1
-#             try:
-#                 db_entry = add_data_to_db(data_to_enter, db)
-#                 list_of_data.append(db_entry)
-#             except:
-#                 cnt += 1
-#                 continue
-#
-#     print(cnt)
-#     print(rows)
-#     db.Session.bulk_save_objects(list_of_data)
-#     db.Session.commit()
-#     db.Session.close()
-#
-
 def add_data_to_db(new_entry, db):
     data_to_enter = modals.DatedLocation(latitude=new_entry["latitude"], longitude=new_entry["longitude"],
                                          assaults=new_entry['assault'], date=new_entry["date"],
@@ -120,7 +27,7 @@ def add_data_to_db(new_entry, db):
 def main():
 
     db = modals.CloudDB()
-
+    Session = db.get_session()
     list_of_data = []
     first_pass = True
     rows = 0
@@ -153,9 +60,9 @@ def main():
             entry = modals.feed_master(data_to_enter)
             list_of_data.append(entry)
 
-    db.Session.bulk_save_objects(list_of_data)
-    db.Session.commit()
-    db.Session.close()
+    Session.bulk_save_objects(list_of_data)
+    Session.commit()
+    Session.close()
 
 
 def add_data_to_db(new_entry, db):
